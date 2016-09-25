3da96f1459ad3627a6d4464e6aba0a39888b3d5e
ALXTNG <alessandro@Alessandros-MacBook-Pro.local>
2016-09-25 22:29:27 +0200

    first commit

--- D50/D50/workinprogress/urls.py
+++ D50/D50/workinprogress/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^404/', views.WIP, name='WIP'),
     url(r'^about/', views.aboutNEXT, name='aboutNEXT'),
     url(r'^logo/', views.logo, name='logo'),
+    url(r'^contact/', views.contact, name='contact'),
+    url(r'^mailinglist/', views.mailinglist, name='mailinglist'),
 ]
--- D50/D50/workinprogress/views.py
+++ D50/D50/workinprogress/views.py
@@ -11,6 +11,12 @@ def aboutNEXT(request):
     
 def logo(request):
     return render(request, 'workinprogress/logo.html')
+    
+def contact(request):
+    return render(request, 'workinprogress/contact.html')
+    
+def mailinglist(request):
+    return render(request, 'workinprogress/mailinglist.html')
 
 #~ def index(request):
     #~ return HttpResponse("Hello, world. You're at the polls index.")
